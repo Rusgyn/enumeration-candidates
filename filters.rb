ceaa38f7390dc3a25d5064667ae2fc554f279e35
rusgyn <rusgynmagsanay@gmail.com>
2024-10-26 14:57:04 -0400

    Takes a single candidate, return true if experience has 2 years or more.

--- filters.rb
+++ filters.rb
@@ -1,12 +1,13 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
-
+require './candidates'
 def find(id)
     # Your code Here
   end
   
   def experienced?(candidate)
     # Your code Here
+    candidate[:years_of_experience] >=2
   end
   
   def qualified_candidates(candidates)
--- main.rb
+++ main.rb
@@ -6,4 +6,11 @@ require './filters'
 
 ## Your test code can go here
 
-pp @candidates
+### Task 1. Experienced Candidates
+#### Takes in a single candidate (hash). Note: not the array.
+#### Returns true if the candidate has 2 years of experience or more
+#### Returns false otherwise
+puts experienced?(@candidates[0])  # Output: TRUE
+puts experienced?(@candidates[1])  # Output: FALSE
+
+#pp @candidates
